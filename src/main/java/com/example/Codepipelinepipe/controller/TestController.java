6a42446b4231fea8f99271dc4bb9aa2f92acc012
ragavendra684 <122417997+ragavendra684@users.noreply.github.com>
2025-06-22 16:39:44 +0530

    Update TestController.java

--- src/main/java/com/example/Codepipelinepipe/controller/TestController.java
+++ src/main/java/com/example/Codepipelinepipe/controller/TestController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+	
 	@GetMapping("/test")
 	public String getResult() {
 		return "Hello Pipeline i got you";
 	}
+
+	@GetMapping("/run")
+	public String getResult() {
+		return "Hello apple, i got you";
+	}
+	
 }
